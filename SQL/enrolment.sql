3e6b17e407724c7c2cd11e8aa1fcb9169d6ed929
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2022-07-04 01:27:23 +0600

    Add files via upload

---
+++ SQL/enrolment.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 03, 2022 at 09:19 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `enrolment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_tbl`
+--
+-- Error reading structure for table enrolment.admin_tbl: #1932 - Table &#039;enrolment.admin_tbl&#039; doesn&#039;t exist in engine
+-- Error reading data for table enrolment.admin_tbl: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `enrolment`.`admin_tbl`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table enrolment.migrations: #1932 - Table &#039;enrolment.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table enrolment.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `enrolment`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table enrolment.personal_access_tokens: #1932 - Table &#039;enrolment.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table enrolment.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `enrolment`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_tbl`
+--
+-- Error reading structure for table enrolment.student_tbl: #1932 - Table &#039;enrolment.student_tbl&#039; doesn&#039;t exist in engine
+-- Error reading data for table enrolment.student_tbl: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `enrolment`.`student_tbl`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers_tbl`
+--
+-- Error reading structure for table enrolment.teachers_tbl: #1932 - Table &#039;enrolment.teachers_tbl&#039; doesn&#039;t exist in engine
+-- Error reading data for table enrolment.teachers_tbl: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `enrolment`.`teachers_tbl`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
